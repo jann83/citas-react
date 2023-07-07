670c9b548622817b736210432a2187bdda2e2028
jann83 <83481345+jann83@users.noreply.github.com>
2023-07-07 15:55:18 -0600

    Create Error.jsx

---
+++ citas-react/src/components/Error.jsx
@@ -0,0 +1,13 @@
+
+
+const error = () => {
+  return (
+    <div className=" bg-red-800  text-white text-center p-3 mb-3 font-bold rounded-md ">
+          <p >Todos los campos son obligatorios</p>
+
+        </div>
+        
+  )
+}
+
+export default error
